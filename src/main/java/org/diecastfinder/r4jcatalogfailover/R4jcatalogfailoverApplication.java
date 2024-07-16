201900701df82214afc59b77265ed3ed24dc5844
Gennady Sitnikov <Gennady_Sitnikov@genpt.com>
2024-07-16 17:54:38 +0200

    Initial commit: just after spring initializer

---
+++ src/main/java/org/diecastfinder/r4jcatalogfailover/R4jcatalogfailoverApplication.java
@@ -0,0 +1,13 @@
+package org.diecastfinder.r4jcatalogfailover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class R4jcatalogfailoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(R4jcatalogfailoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/diecastfinder/r4jcatalogfailover/R4jcatalogfailoverApplicationTests.java
@@ -0,0 +1,13 @@
+package org.diecastfinder.r4jcatalogfailover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class R4jcatalogfailoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
